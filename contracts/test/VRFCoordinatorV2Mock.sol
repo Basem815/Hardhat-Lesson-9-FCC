e58dab04c333c4af246a37715d676283a8176ed2
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-17 10:50:32 +0500

    fix test issues

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -2,5 +2,4 @@
 
 pragma solidity ^0.8.7;
 
-import "../../node_modules/@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
-
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
